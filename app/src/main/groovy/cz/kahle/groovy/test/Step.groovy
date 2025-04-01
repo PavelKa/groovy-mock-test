d178ee1ea74d4fc045cdc67be9f6d095f50837d3
Pavel Kahle <kahle@kahle.cz>
2025-04-01 09:47:03 +0200

    tests

--- app/src/main/groovy/cz/kahle/groovy/test/Step.groovy
+++ app/src/main/groovy/cz/kahle/groovy/test/Step.groovy
@@ -18,4 +18,27 @@ class Step {
     static String staticMethod2(param) {
         return param
     }
+    int getMaxNumberOfRunningInstance(config_deployer_instance, deployerInstance, maxDefault) {
+        def max = null
+        deployerInstance.split(',').each {
+            def xMax = config_deployer_instance?.get(it)?.max
+            if (max == null) {
+
+            }
+            if (xMax != null) {
+                max = maxDefault
+                return max
+            }
+            if (max == null || xMax < max) {
+                max = xMax
+            }
+        }
+        if (max == null) {
+            steps.rbrLog.warn(logParams, "Maximální počet instancí pro $deployerInstance není definován, použije se defaultní hodnota $maxDefault")
+            max = maxDefault
+        } else {
+            steps.rbrLog.info(logParams, "Maximální počet instancí pro $deployerInstance načten z konfigurace, hodnota: $maxDefault")
+        }
+        return max
+    }
 }
